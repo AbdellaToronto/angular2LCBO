bfe4e299c9ddf0a25aed9aa96efabc5da987bf9c
Abdella <abdellaalioncan@gmail.com>
2015-11-15 20:15:35 -0500

    need to fix my init setup, cant quite figure some things out, moving on for now though

--- src/app/components/lcbo-app/lcbo-app.ts
+++
@@ -1,1 +0,0 @@
-import {Component, bootstrap} from 'angular2/angular2';
---
+++ src/lcbo-app/lcbo-app.ts
@@ -0,0 +1,17 @@
+
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+    selector: 'lcbo-app',
+    template: `
+    <h3>Hello world</h3>
+    `
+})
+class LCBOApp {
+    constructor(){
+
+    }
+}
+
+bootstrap(LCBOApp);
